d69b062de58c654536cd49da0d029d7801d1d1c1
hinakot1228 <74905456+hinakot1228@users.noreply.github.com>
2021-07-17 17:27:43 +0900

    welcome画面修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,8 +65,16 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
+            
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Time & Attendance System
+                </div>
+
+                <div class="links">
+                    <a href="{{url('/admin')}}">ADMIN</a>
+                    @if (Route::has('login'))
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -78,21 +86,6 @@
                     @endauth
                 </div>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
