2f999da315d8dca5d95afe514636a50c123ccaa4
hinakot1228 <74905456+hinakot1228@users.noreply.github.com>
2021-07-18 10:25:01 +0900

    time stamp

--- app/Http/Controllers/Work_timeController.php
+++ app/Http/Controllers/Work_timeController.php
@@ -4,21 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Work_time;
+use Carbon\Carbon;
 
 class Work_timeController extends Controller
 {
     //
     public function store(Request $request)
     {
-        dd($request);
+        // dd($request);
         $work_time = new Work_time;
-        $work_time->start_time = '2021-07-17 22:22:00';
+        // $work_time->start_time = '2021-07-17 22:22:00';
         // $work_time->end_time = '2021-07-17 22:22:00';
         // $work_time -> work_time = $request->time;
-        $work_time->status = 'active';
-        $work_time->user_id = 1;
+        // $work_time->status = 'active';
+        // $work_time->user_id = 6;
 
-        $work_time->save();
+        // $work_time->save();
+
+        Work_time::insert(array('start_time'=>Carbon::now(), 'user_id'=>6));
 
         //リダイレクト処理
         // return redirect()->route('/stamp');
